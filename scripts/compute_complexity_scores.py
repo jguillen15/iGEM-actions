b530e33b184cab8e441c36aa9f005c90439cd932
jguillen15 <47996074+jguillen15@users.noreply.github.com>
2024-07-06 17:38:32 -0700

    Add complexity_scores to workflow

---
+++ scripts/compute_complexity_scores.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+import scriptutils
+import sbol_utilities
+from pathlib import Path
+from unittest.mock import patch
+
+error = False
+package = scriptutils.package_dirs()
+
+
+print(f'Calculating complexity scores for {os.path.basename(package)}')
+try:
+    """Test that a command-line invocation of complexity scoring works"""
+    test_dir = Path(__file__).parent
+    test_args = ['calculate_complexity_scores.py',
+                     '--credentials', str(test_dir.parent / 'test_secret_idt_credentials.json'),
+                     package, 'distro_output.nt']
+    with patch.object(sys, 'argv', test_args):
+        sbol_utilities.calculate_complexity_scores.main()
+
+except (OSError, ValueError) as e:
+    print(f'Could not calculate complexity scores for {os.path.basename(package)}: {e}')
+    error = True
+
+# If there was an error, flag on exit in order to notify executing YAML script
+if error:
+    sys.exit(1)
