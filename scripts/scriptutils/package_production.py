e6862d860b47412f53b4eb923860b80b22134349
jguillen15 <47996074+jguillen15@users.noreply.github.com>
2024-08-06 09:45:35 -0700

    Update package_production.py

--- scripts/scriptutils/package_production.py
+++ scripts/scriptutils/package_production.py
@@ -245,8 +245,11 @@ def extract_synthesis_files(root: str, doc: sbol3.Document) -> sbol3.Document:
         if len(insert.sequences) != 1:
             print(sequence_number_warning.format(insert.identity, len(insert.sequences)))
         print(insert.identity)
-        print(insert.sequences[0].lookup().measures)
-        print(type(insert.sequences[0].lookup().measures))
+        uri = insert.identity + "_sequence/Measure1"
+        measures_des = doc.find(uri)
+        print(measures_des.description)
+        #print(insert.sequences[0].lookup().measures)
+        #print(type(insert.sequences[0].lookup().measures))
         insert.sequences[0].lookup().measures = []    
 
     # for GenBank export, copy build products to new Document, omitting ones without sequences
