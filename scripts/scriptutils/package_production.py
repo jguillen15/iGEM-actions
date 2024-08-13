a5a1f1d1ed5a192e9ad181a5f938ccbc1d334ab3
jguillen15 <47996074+jguillen15@users.noreply.github.com>
2024-08-12 21:56:21 -0700

    Print credentials path

--- scripts/scriptutils/package_production.py
+++ scripts/scriptutils/package_production.py
@@ -241,6 +241,7 @@ def extract_synthesis_files(root: str, doc: sbol3.Document) -> sbol3.Document:
     full_constructs = [m.lookup() for m in sorted(build_plan.members)]
     inserts = {c: vector_to_insert(c) for c in full_constructs}  # May contain non-vector full_constructs
 
+    sequence_number_warning = 'Omitting {}: GenBank exports require 1 sequence, but found {}'
     for vector, insert in inserts.items():
         if len(insert.sequences) != 1:
             print(sequence_number_warning.format(insert.identity, len(insert.sequences)))
@@ -253,7 +254,7 @@ def extract_synthesis_files(root: str, doc: sbol3.Document) -> sbol3.Document:
         insert.sequences[0].lookup().measures = []    
 
     # for GenBank export, copy build products to new Document, omitting ones without sequences
-    sequence_number_warning = 'Omitting {}: GenBank exports require 1 sequence, but found {}'
+    #sequence_number_warning = 'Omitting {}: GenBank exports require 1 sequence, but found {}'
     build_doc = sbol3.Document()
     components_copied = set(full_constructs)  # all of these will be copied directly in the next iterator
     n_genbank_constructs = 0
